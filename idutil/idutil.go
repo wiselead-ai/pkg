38766daecc2e826b3a3fe0e71f70bfd78654fb27
Alessandro Resta <alessandro.resta@proton.me>
2025-01-11 12:55:17 +0200

    Improve error handling in NewID function for ULID generation

--- idutil/idutil.go
+++ idutil/idutil.go
@@ -1,6 +1,7 @@
 package idutil
 
 import (
+	"fmt"
 	"time"
 
 	"math/rand"
@@ -8,13 +9,14 @@ import (
 	"github.com/oklog/ulid/v2"
 )
 
+// NewID is a helper function to generate a new ULID string.
 func NewID() (string, error) {
 	id, err := ulid.New(
 		ulid.Timestamp(time.Now()),
 		rand.New(rand.NewSource(time.Now().UnixNano())),
 	)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("could not generate new ULID: %w", err)
 	}
 	return id.String(), nil
 }
